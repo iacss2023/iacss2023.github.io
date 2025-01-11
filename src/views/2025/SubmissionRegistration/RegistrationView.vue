c7a7e719ef375d6c48a5f2ffcf3a727a23fed6ca
Xiangtong Chu <sheatoncxt98@163.com>
2025-01-11 20:54:47 +0800

    update register fee

--- src/views/2025/SubmissionRegistration/RegistrationView.vue
+++ src/views/2025/SubmissionRegistration/RegistrationView.vue
@@ -3,23 +3,23 @@
     <div class="view-container">
       <div class="view-title">Registration Information</div>
       <div class="view-main with-setting">
-        Will come soon.
-<!--        <div class="view-section">-->
-<!--          <div class="view-section-title">Registration Fee</div>-->
-<!--          <el-table :data="feeInfo" stripe>-->
-<!--            <el-table-column label="">-->
-<!--              <template v-slot="scope"><p>{{scope.row.affair}}</p></template>-->
-<!--            </el-table-column>-->
-<!--            <el-table-column label="Early Bird* (USD)" align="center">-->
-<!--              <template v-slot="scope"><p style="text-align: center;">{{scope.row.earlyBird}}</p></template>-->
-<!--            </el-table-column>-->
-<!--            <el-table-column label="Standard Fee (USD)" align="center">-->
-<!--              <template v-slot="scope"><p style="text-align: center">{{scope.row.standard}}</p></template>-->
-<!--            </el-table-column>-->
-<!--          </el-table>-->
-<!--          <p class="note title" style="margin-top: 10px">*EARLY-BIRD REGISTRATION ENDS 30.06.2025</p>-->
-<!--          <p class="note">Ad Exhibitors: Only allowed to the stand staff of an exhibiting company. As the booth rental automatically includes one full registration per booth, exhibitors will be allowed to register one additional person per single booth, provided the person is working on the booth.</p>-->
-<!--        </div>-->
+<!--        Will come soon.-->
+        <div class="view-section">
+          <div class="view-section-title">Registration Fee</div>
+          <el-table :data="feeInfo" stripe>
+            <el-table-column label="">
+              <template v-slot="scope"><p>{{scope.row.affair}}</p></template>
+            </el-table-column>
+            <el-table-column label="Early Bird* (JPY)" align="center">
+              <template v-slot="scope"><p style="text-align: center;">¥{{scope.row.earlyBird}}</p></template>
+            </el-table-column>
+            <el-table-column label="Standard Fee (JPY)" align="center">
+              <template v-slot="scope"><p style="text-align: center">¥{{scope.row.standard}}</p></template>
+            </el-table-column>
+          </el-table>
+          <p class="note title" style="margin-top: 10px">*EARLY-BIRD REGISTRATION ENDS 30.06.2025</p>
+          <p class="note">Ad Exhibitors: Only allowed to the stand staff of an exhibiting company. As the booth rental automatically includes one full registration per booth, exhibitors will be allowed to register one additional person per single booth, provided the person is working on the booth.</p>
+        </div>
       </div>
     </div>
   </div>
@@ -32,12 +32,12 @@ export default {
   data () {
     return {
       feeInfo: [
-        { affair: 'Full – Non-member', earlyBird: 340, standard: 370 },
-        { affair: 'Full - Member', earlyBird: 320, standard: 340 },
-        { affair: 'Student - Non-member', earlyBird: 220, standard: 240 },
-        { affair: 'Student - Member', earlyBird: 190, standard: 210 },
-        { affair: 'Exhibitors', earlyBird: 250, standard: 270 },
-        { affair: 'Accompanying Person', earlyBird: 120, standard: 150 }
+        { affair: 'Full – Non-member', earlyBird: '40,000', standard: '45,000' },
+        { affair: 'Full - Member', earlyBird: '35,000', standard: '40,000' },
+        { affair: 'Student - Non-member', earlyBird: '30,000', standard: '40,000' },
+        { affair: 'Student - Member', earlyBird: '15,000', standard: '20,000' },
+        { affair: 'Exhibitors', earlyBird: '10,000', standard: '15,000' },
+        { affair: 'Accompanying Person', earlyBird: '15,000', standard: '20,000' }
       ]
     }
   },
