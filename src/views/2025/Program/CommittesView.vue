d3e71b5c2c7ff575735f29dc489cc9c727e31915
sheaton1 <819022816@qq.com>
2025-01-14 00:57:52 +0800

    update committes

--- src/views/2025/Program/CommittesView.vue
+++ src/views/2025/Program/CommittesView.vue
@@ -23,9 +23,14 @@
                 <p>{{people.institution}}</p>
               </div>
               <div class="view-section" style="padding: 0"></div>
-              <div v-for="people in committee.others" :key="people.people" class="view-section" style="border-bottom: none">
+              <div v-for="people in committee.others" :key="people.work" class="view-section" style="border-bottom: none">
                 <div class="view-section-title small">{{people.work}}</div>
-                <p>{{people.people}} ({{people.institution}})</p>
+                <div v-if="people.people">{{people.people}} ({{people.institution}})</div>
+                <div v-else>
+                  <div v-for="p in people.peoples" :key="p.name">
+                    <p>{{p.name}} ({{p.institution}})</p>
+                  </div>
+                </div>
               </div>
             </el-tab-pane>
           </el-tabs>
@@ -54,12 +59,42 @@ export default {
           { work: 'Treasures: Registration, Membership and Website', people: 'Mr. Takashi Fukushima', institution: 'Special-appointed Assistant Professor, Tokyo Gakugei University' },
           { work: 'Public Relations', people: 'Dr. Naoki Suzuki', institution: 'Associate Professor, Tokyo Gakugei University' },
           { work: 'Coordinator for publishing the conference book', people: 'Dr. Walter Ho', institution: 'Visiting Scholar, Tokyo Gakugei University' },
-          { work: 'Coordinator for organizing the sessions', people: 'Dr. Dhin-Ichiro Moriyama', institution: 'Associate Professor, Tokyo Gakugei University' },
-          { work: 'Coordinator for the registration on site & managing the staff', people: 'Mr. Koji Ishii', institution: 'Assistant Professor, Utsunomiya University' },
+          {
+            work: 'Coordinator for organizing the sessions',
+            peoples:
+              [{ name: '*Invited sessions_Dr. Shin-Ichiro Moriyama', institution: 'Associate Professor, Tokyo Gakugei University' },
+                { name: '*PE(Oral)_Dr. Walter Ho', institution: 'Visiting Scholar, Tokyo Gakugei University' },
+                { name: '*Sports (Oral)_Dr. Seiji Hirosawa', institution: 'Specially-appointed Lecturer, Toin Yokohama University' },
+                { name: '*Poster_ Mr. Koji Ishii', institution: 'Assistant Professor, Utsunomiya University' }]
+          },
+          {
+            work: 'Coordinator for the registrations',
+            peoples:
+              [{ name: 'Mr. Shin-Ichiro Sewa', institution: 'Teacher, Matsudo Municipal High School' },
+                { name: 'Mr. Ryoji Fujiwara', institution: 'Teacher, Sakado High School attached to the University of Tsukuba' }]
+          },
+          { work: 'Coordinator for the sponsorship & exhibition', people: 'Mr. Koji Ishii', institution: 'Assistant Professor, Utsunomiya University' },
+          { work: 'Coordinator for managing the staff', people: 'Mr. Koji Ishii', institution: 'Assistant Professor, Utsunomiya University' },
           { work: 'Coordinator with other organization <hotel et.al.,>', people: 'Dr. Seiji Hirosawa', institution: 'Specially-appointed Lecturer, Toin Yokohama University' },
-          { work: 'Coordinator for Lunch and Coffee Break', people: 'Mr. Ryoji Fujiwara', institution: 'Teacher, Sakado High School attached to the University of Tsukuba' },
-          { work: 'Coordinator for Excursion', people: 'Mr. Shin-Ichiro Sewa', institution: 'Teacher, Teacher, Matsudo Municipal High School' },
-          { work: 'Coordinator for Ceremonies', people: 'Mr. Yoshiko Haoka', institution: 'Associate Professor, Tokyo Seitoku University' }
+          {
+            work: 'Coordinator for Lunch and Coffee Break',
+            peoples:
+              [{ name: 'Mr. Ryoji Fujiwara', institution: 'Teacher, Sakado High School attached to the University of Tsukuba' },
+                { name: 'Mr. Shin-Ichiro Sewa', institution: 'Teacher, Matsudo Municipal High School' }]
+          },
+          {
+            work: 'Coordinator for Reception & Dinner',
+            peoples:
+              [{ name: 'Ms. Yoshiko Haoka', institution: 'Associate Professor, Tokyo Seitoku University' },
+                { name: 'Ms. Mizuho Shiozaki', institution: 'Professor, Akikusa Gakuen University' }]
+          },
+          { work: 'Coordinator for Excursion', people: 'Mr. Takashi Fukushima', institution: 'Special-appointed Assistant Professor, Tokyo Gakugei University' },
+          {
+            work: 'Coordinator for Ceremonies',
+            peoples:
+              [{ name: 'Ms. Mizuho Shiozaki', institution: 'Professor, Akikusa Gakuen University' },
+                { name: 'Mr. Yoshiko Haoka', institution: 'Associate Professor, Tokyo Seitoku University' }]
+          }
         ],
         scientific: [
           { people: 'Assoc. Prof. Dr. Naoki Suzuki', institution: 'Tokyo Gakugei University, Japan' },
