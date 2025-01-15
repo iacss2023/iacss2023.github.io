ae027f98daedc54198ea5b29221b1509d4e8cf22
Xiangtong Chu <sheatoncxt98@163.com>
2025-01-15 10:22:11 +0800

    update program schedule

--- src/views/2025/Program/ProgramView.vue
+++ src/views/2025/Program/ProgramView.vue
@@ -3,7 +3,18 @@
     <div class="view-container">
       <div class="view-title">Program</div>
       <div class="view-main">
-        Will coming soon.
+          <el-table :data="programSchedule" stripe>
+            <el-table-column align="center">
+              <template v-slot="scope">
+                <p>{{scope.row.program}}</p>
+              </template>
+            </el-table-column>
+            <el-table-column align="center" width="250">
+              <template v-slot="scope">
+                <p>{{scope.row.time}}</p>
+              </template>
+            </el-table-column>
+          </el-table>
       </div>
     </div>
   </div>
@@ -14,6 +25,13 @@ export default {
   name: 'ProgramView',
   data () {
     return {
+      programSchedule: [
+        { program: 'Registration', time: 'Friday, 12.09.2025' },
+        { program: 'Visiting IJSS, Opening Ceremony, Commemorative Keynote Speech, and Welcome Reception', time: 'Saturday, 13.09.2025' },
+        { program: 'Full-day Conference Sessions', time: 'Sunday, 14.09.2025' },
+        { program: 'Full-day Conference Sessions and Closing Ceremony', time: 'Monday, 15.09.2025' },
+        { program: 'Visits and Exchange Activities', time: 'Tuesday, 16.09.2025' }
+      ]
     }
   }
 }
